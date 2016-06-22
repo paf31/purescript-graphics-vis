b748b860f9cfaad51b6c0a93ca32c2004ae36c37
Phil Freeman <paf31@cantab.net>
2016-06-21 20:52:32 -0700

    Fix canvas dimensions

--- src/Graphics.js
+++ src/Graphics.js
@@ -5,11 +5,9 @@ var canvas;
 exports.createCanvas = function() {
   if (!canvas) {
     canvas = document.createElement("canvas");
-    canvas.width = window.width;
-    canvas.height = window.height;
-    canvas.style.width  = window.width + 'px';
-    canvas.style.height = window.height + 'px';
     document.body.appendChild(canvas);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
   }
   return canvas;
 };
