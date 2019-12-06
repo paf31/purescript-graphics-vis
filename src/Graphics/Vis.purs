7eefd14163a652890eba6f4cf942d2417d06cd04
Franklin Chen <franklinchen@franklinchen.com>
2019-12-06 17:31:42 -0500

    Update to drawing 4 and PureScript 0.13. (#5)
    * Update to drawing 4 and PureScript 0.13.
    
    * Update to drawing 4 and PureScript 0.13.

--- src/Graphics/Vis.purs
+++ src/Graphics/Vis.purs
@@ -15,12 +15,12 @@ module Graphics.Vis
 import Prelude
 import Graphics.Drawing as Graphics.Drawing
 import Color (Color, hsl)
-import Control.Monad.Eff (Eff)
+import Effect (Effect)
 import Data.Array (drop, length, take)
 import Data.Foldable (sum)
 import Data.Int (toNumber)
 import Data.Monoid (class Monoid, mempty) as Data.Monoid
-import Graphics.Canvas (CANVAS, CanvasElement, getContext2D, getCanvasWidth, getCanvasHeight, setFillStyle, save, restore, fillRect)
+import Graphics.Canvas (CanvasElement, getContext2D, getCanvasWidth, getCanvasHeight, setFillStyle, save, restore, fillRect)
 import Graphics.Drawing (Drawing, Shape, render, circle, translate, outlined, outlineColor, lineWidth, filled)
 import Math (Radians, abs, acos, asin, atan, atan2, ceil, cos, e, exp, floor, ln10, ln2, log, log10e, log2e, pi, pow, remainder, round, sin, sqrt, sqrt1_2, sqrt2, tan, trunc, (%))
 
@@ -28,31 +28,29 @@ import Math (Radians, abs, acos, asin, atan, atan2, ceil, cos, e, exp, floor, ln
 type FFT = Array Number
 
 foreign import animateImpl
-  :: forall eff
-   . (Number -> FFT -> Eff eff Unit)
-   -> Eff eff Unit
+  :: (Number -> FFT -> Effect Unit)
+   -> Effect Unit
 
 foreign import createCanvas
-  :: forall eff
-   . Eff (canvas :: CANVAS | eff) CanvasElement
+  :: Effect CanvasElement
 
 -- | Render a drawing to the browser canvas, replacing any
 -- | existing drawing.
-graphics :: forall eff. Drawing -> Eff (canvas :: CANVAS | eff) Unit
+graphics :: Drawing -> Effect Unit
 graphics scene' = do
   canvas <- createCanvas
   context <- getContext2D canvas
   width <- getCanvasWidth canvas
   height <- getCanvasHeight canvas
   _ <- save context
-  _ <- setFillStyle "rgba(255,255,255,0.1)" context
-  _ <- fillRect context { x: 0.0, y: 0.0, w: width, h: height }
+  _ <- setFillStyle context "rgba(255,255,255,0.1)"
+  _ <- fillRect context { x: 0.0, y: 0.0, width: width, height: height }
   _ <- restore context
   render context scene'
 
 -- | Render an animation which depends on the current time
 -- | and audio input.
-animate :: forall eff. (Number -> FFT -> Drawing) -> Eff (canvas :: CANVAS | eff) Unit
+animate :: (Number -> FFT -> Drawing) -> Effect Unit
 animate f = animateImpl (\t fft -> graphics (f t fft))
 
 average :: FFT -> Number
